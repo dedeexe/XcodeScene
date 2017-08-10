e9e331a7ba09dc89f4cd9de5b85da54286201cee
dedeexe <dede.exe@gmail.com>
2017-08-09 23:03:17 -0300

    add: default keyboard behavior

--- viper/ViperScene.xctemplate/___FILEBASENAME___ViewController.swift
+++ viper/ViperScene.xctemplate/___FILEBASENAME___ViewController.swift
@@ -14,13 +14,12 @@ class ___FILEBASENAME___ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         assertDependencies()
-    }
+    	setup()
+	}
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     
@@ -31,11 +30,52 @@ class ___FILEBASENAME___ViewController: UIViewController {
     fileprivate func assertDependencies() {
         assert(presenter != nil, "Did not set Presenter to the view")
     }
+
+	fileprivate func setup() {
+        startObservers()
+	}
+    
+    deinit {
+        stopObservers()
+    }
 }
 
-//MARK: - View Delegate
+// MARK: - View Delegate
 extension ___FILEBASENAME___ViewController : ___FILEBASENAME___View {
     func show(something: String) {
         //Implement what to show here
+    }
+}
+
+// MARK: - Configurations
+extension ___FILEBASENAME___ViewController { 
+    func startObservers() {
+        let nc = NotificationCenter.default
+        nc.addObserver(self, selector: #selector(showKeyboard), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        nc.addObserver(self, selector: #selector(hideKeyboard), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func stopObservers() {
+        let nc = NotificationCenter.default
+        nc.removeObserver(self, name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        nc.removeObserver(self, name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func showKeyboard(notification:Notification) {
+        if let frame = notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? NSValue {
+            let finalFrame = frame.cgRectValue
+            let frameHeight = finalFrame.height
+            
+            // Implement keyboard show behavior here
+        }
+    }
+    
+    func hideKeyboard(notification:Notification) {
+        if let frame = notification.userInfo?["UIKeyboardBoundsUserInfoKey"] as? NSValue {
+            let finalFrame = frame.cgRectValue
+            let frameHeight = finalFrame.height
+            
+            // Implement keyboard hide behavior here
+        }
     }
 }
